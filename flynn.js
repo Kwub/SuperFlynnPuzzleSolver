1c4d674e8168e48aaadd0714c0a0e30bf71cde56
Daniel Wei <danielwei816@gmail.com>
2019-10-17 10:02:26 -0400

    parsed words into an array

--- flynn.js
+++ flynn.js
@@ -2,4 +2,8 @@ var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var xmlhttp=new XMLHttpRequest();
 xmlhttp.open('GET', "https://raw.githubusercontent.com/dwyl/english-words/master/words_alpha.txt", false);
 xmlhttp.send();
-console.log(xmlhttp.responseText)
+var words = xmlhttp.responseText.split('\n');
+for (var i = 0; i < words.length; i++) {
+    console.log(words[i]);
+}
+// console.log(xmlhttp.responseText)
