c73fbd8357069b7d60a87a61f523b73fffee99bd
Daniel Wei <danielwei816@gmail.com>
2019-10-17 09:46:10 -0400

    after merge

--- flynn.js
+++ flynn.js
@@ -1,1 +1,5 @@
-alert("Hi")
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+var xmlhttp=new XMLHttpRequest();
+xmlhttp.open('GET', "https://raw.githubusercontent.com/dwyl/english-words/master/words_alpha.txt", false);
+xmlhttp.send();
+console.log(xmlhttp.responseText)
