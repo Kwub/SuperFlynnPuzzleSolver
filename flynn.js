906cb70315dc0c96d972b6533d1b892ceee97fd2
Daniel Wei <danielwei816@gmail.com>
2019-10-21 10:22:30 -0400

    found all ay words

--- flynn.js
+++ flynn.js
@@ -1,34 +1,46 @@
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-var listRequest = new XMLHttpRequest();
+let listRequest = new XMLHttpRequest();
 listRequest.open('GET', "https://raw.githubusercontent.com/dwyl/english-words/master/words_alpha.txt", false);
 listRequest.send();
 
-var wordsList = listRequest.responseText.split('\n');
+let wordsList = listRequest.responseText.split('\n');
+let filteredList = [];
 
-for (var i = 0; i < wordsList.length; i++) {
-    console.log(wordsList[i]);
+for (let i = 0; i < wordsList.length; i++) {
+    
+    let word = wordsList[i]
+    word = word.replace("\r", "")
+   
+    if (word.endsWith("ay")) {
+        filteredList.push(word)
+    }
+   
+}
+
+for (let i = 0; i < filteredList.length; i++) {
+    console.log(filteredList[i])
 }
 
 // can only access words 1000 times per day or we need a new key
 function accessAPI(wordQuery) {
-    var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+    let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-    var wordRequest = new XMLHttpRequest();
-    var wordString = wordQuery;
-    var key // = "2db3f398-ea01-49b9-a7a0-78142800d42a";
-    var wordRequestURL = "https://www.dictionaryapi.com/api/v3/references/collegiate/json/" + wordString + "?key=" + key;
+    let wordRequest = new XMLHttpRequest();
+    let wordString = wordQuery;
+    let key // = "2db3f398-ea01-49b9-a7a0-78142800d42a";
+    let wordRequestURL = "https://www.dictionaryapi.com/api/v3/references/collegiate/json/" + wordString + "?key=" + key;
 
     wordRequest.open('GET', wordRequestURL, false);
     wordRequest.send();
 
-    var jsonString = wordRequest.responseText
+    let jsonString = wordRequest.responseText
     jsonString = jsonString.toLowerCase();
-    var word = JSON.parse(jsonString);
+    let word = JSON.parse(jsonString);
 
-    for (var i = 0; i < word.length; i++) {
-        var match = false;
-        for (var j = 0; j < word[i].shortdef.length; j++) {
+    for (let i = 0; i < word.length; i++) {
+        let match = false;
+        for (let j = 0; j < word[i].shortdef.length; j++) {
             if (word[i].shortdef[j].includes("drink") || word[i].shortdef[j].includes("beverage") || word[i].shortdef[j].includes("juice") || word[i].shortdef[j].includes("milk")) {
                 match = true;
             }
